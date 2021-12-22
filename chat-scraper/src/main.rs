08ab87ab84a25a1b772d1ec3d7ee6f10382428ed
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-12-22 16:09:30 -0700

    rustfmt

--- chat-scraper/src/main.rs
+++ chat-scraper/src/main.rs
@@ -1,19 +1,16 @@
+use lazy_static::lazy_static;
 use linemux::MuxedLines;
+use regex::Regex;
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader};
-use regex::Regex;
-use lazy_static::lazy_static;
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-
     let f: &'static str = "/home/arunscape/Downloads/mc118/data/logs/latest.log";
 
     let mut lines = MuxedLines::new()?;
 
     lines.add_file(&f).await?;
-
 
     while let Ok(Some(line)) = lines.next_line().await {
         // dbg!(line.line());
@@ -23,11 +20,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     Ok(())
 }
 
-
 struct Command {
     username: String,
     action: Action,
-
 }
 
 enum Action {
@@ -45,39 +40,33 @@ struct Transaction {
 }
 
 fn parse_chat_msg(line: &str) {
-   // [05:49:01] [Async Chat Thread - #11/INFO]: <Arunscape> hello
-   
-   if let Some((time, msg)) = line.split_once(": "){
+    // [05:49:01] [Async Chat Thread - #11/INFO]: <Arunscape> hello
+
+    if let Some((time, msg)) = line.split_once(": ") {
         // dbg!(&time);
         if !is_chat(time) {
-        return
+            return;
         }
 
         // it matched
         // dbg!(&msg);
 
-
         let (username, msg) = msg.split_once(" ").unwrap();
 
-        if !msg.starts_with("atb "){
-            return
+        if !msg.starts_with("atb ") {
+            return;
         }
 
-        let username = &username[1..username.len()-1];
+        let username = &username[1..username.len() - 1];
 
         println!("ok, parsing message");
 
         dbg!(&username);
         dbg!(&msg);
-
-
     }
-
-
-
 }
 
-fn is_chat(s: &str) -> bool{
+fn is_chat(s: &str) -> bool {
     // prevent compiling regex in a loop
     dbg!(&s);
     lazy_static! {
