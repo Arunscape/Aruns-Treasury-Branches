aaa06d905169d4da8b2874d708bbad174da19dfc
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-12-22 17:14:10 -0700

    rcon works

--- chat-scraper/src/main.rs
+++ chat-scraper/src/main.rs
@@ -1,12 +1,22 @@
 use lazy_static::lazy_static;
 use linemux::MuxedLines;
+use rcon::Connection;
 use regex::Regex;
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader};
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let f: &'static str = "/home/arunscape/Downloads/mc118/data/logs/latest.log";
+    let rcon_addr = "0.0.0.0:25575";
+
+    let rcon_passwd = std::env::var("RCON_PASSWD")?;
+
+    let mut conn = Connection::builder()
+        .enable_minecraft_quirks(true)
+        .connect(rcon_addr, &rcon_passwd)
+        .await?;
 
     let mut lines = MuxedLines::new()?;
 
@@ -14,7 +24,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     while let Ok(Some(line)) = lines.next_line().await {
         // dbg!(line.line());
-        parse_chat_msg(line.line());
+        parse_chat_msg(line.line(), &mut conn).await;
     }
 
     Ok(())
@@ -39,12 +49,13 @@ struct Transaction {
     amount: usize,
 }
 
-fn parse_chat_msg(line: &str) {
+async fn parse_chat_msg(line: &str, conn: &mut Connection<TcpStream>) {
     // [05:49:01] [Async Chat Thread - #11/INFO]: <Arunscape> hello
 
     if let Some((time, msg)) = line.split_once(": ") {
         // dbg!(&time);
         if !is_chat(time) {
+            println!("Ignoring log item which is not a message");
             return;
         }
 
@@ -54,6 +65,7 @@ fn parse_chat_msg(line: &str) {
         let (username, msg) = msg.split_once(" ").unwrap();
 
         if !msg.starts_with("atb ") {
+            println!("Ignoring message which does not start with atb");
             return;
         }
 
@@ -63,6 +75,10 @@ fn parse_chat_msg(line: &str) {
 
         dbg!(&username);
         dbg!(&msg);
+
+        let res = conn.cmd("say Welcome to Arun's Treasury Branches!").await;
+
+        dbg!(&res);
     }
 }
 
