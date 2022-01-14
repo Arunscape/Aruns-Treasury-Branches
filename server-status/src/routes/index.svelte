f66e3905869a44d301af4ba208d9576835be7266
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-01-13 20:43:16 -0700

    let's see if this works

--- server-status/src/routes/index.svelte
+++ server-status/src/routes/index.svelte
@@ -1,14 +1,39 @@
 <script>
-    import {onMount} from 'svelte'
-    let data;
+	import { onMount } from 'svelte';
 
-    onMount(async () => {
-    const res = await fetch(`https://status.minecraft.woosaree.xyz/api/info`);
-    data = await res.json();
-    console.log(data)
-  });
+	// onMount(async () => {
+	// 	const res = await fetch(`https://status.minecraft.woosaree.xyz/api/info`, {
+	// 		mode: 'no-cors' // 'cors' by default
+	// 	});
+	// 	data = await res.json();
+	// 	console.log(data);
+	// });
+
+	const data = fetch('https://status.minecraft.woosaree.xyz/api/info').then((res) => res.json());
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div>{JSON.stringify(data)}</div>
+<h1>minecraft.woosaree.xyz status page</h1>
+<!-- <div>{JSON.stringify(data)}</div> -->
+{#await data}
+	<div>loading...</div>
+{:then d}
+	<div>
+		<img src={d.favicon} />
+		<div>{d.num_online}/{d.max_players} Players online</div>
+		<div>
+			<div>Players preview</div>
+			{#each d.players_sample as p}
+				<div>p.name</div>
+			{/each}
+		</div>
+
+		<div>Version: {d.version}</div>
+
+		<div>
+			<div>Additional info</div>
+			<div>
+				<div>Ping from {d.ping.from} to minecraft.woosaree.xyz is {d.ping.time}</div>
+			</div>
+		</div>
+	</div>
+{/await}
