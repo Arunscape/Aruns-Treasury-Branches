af91687e2c92b01f2a5139ed6df55f9ed3584dbb
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-01-14 15:42:00 -0700

    hey Nigel here is where you add CSS

--- server-status/src/routes/index.svelte
+++ server-status/src/routes/index.svelte
@@ -1,24 +1,37 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 
-	// onMount(async () => {
-	// 	const res = await fetch(`https://status.minecraft.woosaree.xyz/api/info`, {
-	// 		mode: 'no-cors' // 'cors' by default
-	// 	});
-	// 	data = await res.json();
-	// 	console.log(data);
-	// });
+	type Player = {
+		name: string;
+		id: string;
+	};
+	type PingResponse = {
+		ping: {
+			from: string;
+			time: number;
+		};
+		max_players: number;
+		players_sample: Player[];
+		favicon: string;
+		version: string;
+		protocol: string;
+		description: string;
+		num_online: number;
+	};
 
 	const data = fetch('https://status.minecraft.woosaree.xyz/api/info').then((res) => res.json());
 </script>
 
-<h1>minecraft.woosaree.xyz status page</h1>
+<h1 class="changecolour">minecraft.woosaree.xyz status page</h1>
 <!-- <div>{JSON.stringify(data)}</div> -->
 {#await data}
 	<div>loading...</div>
 {:then d}
 	<div>
-		<img src={d.favicon} />
+		<img
+			src={d.favicon}
+			alt="Server favicon. This shows up in the server list when you connect to a multiplayer server in the minecraft client."
+		/>
 		<div>{d.num_online}/{d.max_players} Players online</div>
 		<div>
 			<div>Players preview</div>
@@ -39,3 +52,20 @@
 		</div>
 	</div>
 {/await}
+
+<style>
+	/* @Nigel the CSS goes here */
+	/* this style I added is trash  so pls delete it */
+	.changecolour {
+		animation: pulse 5s infinite;
+	}
+
+	@keyframes pulse {
+		0% {
+			background-color: #001f3f;
+		}
+		100% {
+			background-color: #ff4136;
+		}
+	}
+</style>
