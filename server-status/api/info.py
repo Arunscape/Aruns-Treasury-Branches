843f09db94f621e111fc11a588e09f31f7d1d81c
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-01-14 15:46:08 -0700

    Access-Control-Allow-Origin *

--- server-status/api/info.py
+++ server-status/api/info.py
@@ -35,6 +35,7 @@ class handler(BaseHTTPRequestHandler):
 
         self.send_response(200)
         self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
         response = get_stats()
         self.wfile.write(json.dumps(response).encode())
