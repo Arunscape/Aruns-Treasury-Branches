ac15d63e2330146051a81e8f640edc13e0b5f3e4
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-01-13 18:34:07 -0700

    let's try it

--- server-status/api/init.py
+++ server-status/api/init.py
@@ -1,12 +1,19 @@
 from http.server import BaseHTTPRequestHandler
+from mcstatus import MinecraftServer
+import json
 
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
+
+        server = MinecraftServer.lookup("minecraft.woosaree.xyz")
+
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header("Content-type", "application/json")
         self.end_headers()
-        message = "hello from python"
-        self.wfile.write(message.encode())
+        response = {
+            'status': server.status(),
+            'ping': server.ping(),
+        }
+        self.wfile.write(json.dumps(response))
         return
