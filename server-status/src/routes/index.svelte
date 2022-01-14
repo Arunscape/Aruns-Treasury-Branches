6e9fbbbc7284e2695b81d780b16ed3449a3cd43f
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-01-13 20:12:10 -0700

    add status page

---
+++ server-status/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {onMount} from 'svelte'
+    let data;
+
+    onMount(async () => {
+    const res = await fetch(`https://status.minecraft.woosaree.xyz/api/info`);
+    data = await res.json();
+    console.log(data)
+  });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>{JSON.stringify(data)}</div>
