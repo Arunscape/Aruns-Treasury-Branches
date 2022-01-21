7087b6059cba1abc9919f087bed9eba565fc89c8
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-01-21 00:15:16 -0700

    try creating plugin

---
+++ plugin/src/main/kotlin/ArunsTreasuryBranches.kt
@@ -0,0 +1,7 @@
+import events.PlayerJoin
+import org.bukkit.plugin.java.JavaPlugin
+class Plugin : JavaPlugin(){
+    override fun onEnable() {
+        server.pluginManager.registerEvents(PlayerJoin(), this)
+    }
+}
