9e7bb30abb677dcfccdf13efabf34e14b0e466e2
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-01-24 22:09:08 -0700

    hey it works with kotlin

---
+++ mc-plugin/src/main/java/xyz/woosaree/arunstreasurybranches/ArunsTreasuryBranches.kt
@@ -0,0 +1,14 @@
+package xyz.woosaree.arunstreasurybranches
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ArunsTreasuryBranches : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+        println("hello from kotlin")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
