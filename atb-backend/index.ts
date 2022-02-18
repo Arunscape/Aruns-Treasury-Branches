55b15e64693e55eacd217452411e7bfa790e50ab
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-02-17 23:47:01 -0700

    some db functions

--- atb-backend/index.ts
+++ atb-backend/index.ts
@@ -1,30 +1,144 @@
 import express from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://bkiuhzngtkoliiqqqunp.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDMwMTkyNCwiZXhwIjoxOTU1ODc3OTI0fQ.vl7VuWJSjhOXRB0ufzMwuzMIg3HKmsHzELJZZpE87EQ";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-const unix_seconds = () => Math.floor(Date.now() / 1000);
+import { Account, Prisma, PrismaClient, Transaction, User } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
 
-io.on("connection", (socket) => {
-    // ...
-});
+type cuid = string;
+
+async function get_accounts(owner_id: cuid): Promise<Account[]> {
+    return await prisma.account.findMany({
+        where: {
+            owner_id
+        }
+    })
+}
+
+async function create_account(owner_id: cuid): Promise<Account | null> {
+    return await prisma.account.create({
+        data: {
+            owner_id,
+            balance: {},
+        }
+    })
+}
+
+// @throws if account has a nonzero balance
+async function delete_account(id: cuid): Promise<Account> {
+
+    return await prisma.$transaction(async (prisma) => {
+        const { balance } = await prisma.account.findFirst({
+            where: {
+                id
+            }
+        });
+
+        for (const [_, value] of Object.entries(balance)) {
+            if (value != 0) {
+                throw new Error("Nonzero balance")
+            }
+        }
+        return prisma.account.delete({
+            where: {
+                id
+            }
+        });
+
+    });
+}
+
+// @throws if user already exists
+async function create_user(username: string, email: string): Promise<User> {
+    return await prisma.user.create({
+        data: {
+            username,
+            email,
+        }
+    })
+}
+
+// @throws if balance is nonzero
+async function delete_user(user_id: cuid): Promise<User> {
+    return await prisma.user.delete({
+        where: {
+            id: user_id
+        }
+    })
+}
+
+// @throws if insufficient balance
+// todo: actually, the balance json object will also need to keep track of other attributes  (nbt data)
+// so can't just add them
+async function create_transaction(from_account_id: cuid, to_account_id: cuid, item_id: number, amount: bigint, other_attributes: Prisma.JsonValue): Promise<Transaction> {
+
+    return await prisma.$transaction(async (prisma) => {
+
+        const from: Account = await prisma.account.findFirst({
+            where: {
+                id: from_account_id
+
+            }
+        });
+
+        if (from.balance[item_id] < amount){
+            throw Error("Insufficient balance")
+        }
+
+        await prisma.account.update({
+            data: {
+                balance: {
+                    // @ts-ignore
+                    ...from.balance,
+                    [item_id]: from.balance[item_id] - amount
+                }
+            },
+            where: {
+                id: from_account_id
+            }
+        });
+
+        const to: Account = await prisma.account.findFirst({
+            where: {
+                id: to_account_id
+
+            }
+        });
+
+        await prisma.account.update({
+            data: {
+                balance: {
+                    // @ts-ignore
+                    ...to.balance,
+                    [item_id]: to.balance[item_id] + amount
+                }
+            },
+            where: {
+                id: to_account_id
+            }
+        });
+
+        return await prisma.transaction.create({
+            data: {
+                from_account_id,
+                to_account_id,
+                item_id,
+                amount,
+                other_attributes
+            }
+        });
+    })
+
+
+}
+
+
 
 app.get('/balance', async (req, res) => {
-    let r = await supabase.from('accounts').select("*")
-
-    console.log(r)
+    let r = await prisma.user.findMany({})
 
     res.json(r)
-
-
 })
 
 
