de684e1a14e7999dece4d8c90caa503849b72e2f
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-05-10 21:07:57 -0600

    ahhh what I'm looking for is apparently called a time series database

--- atb-backend/dbinit.sql
+++ atb-backend/dbinit.sql
@@ -1,26 +1,36 @@
-CREATE TABLE users (
-    id UUID PRIMARY KEY -- minecraft uuid
-    email STRING UNIQUE NOT NULL
-);
 
+-- For a time column expressed as the number of milliseconds since the UNIX epoch, set chunk_time_interval to 24 hour
+-- SELECT set_chunk_time_interval('conditions', 86400000);
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    -- minecraft uuid
+    email TEXT UNIQUE NOT NULL
+);
 -- a user can have multiple accounts
 CREATE TABLE accounts (
-    id UUID PRIMARY KEY
-    userid UUID NOT NULL REFERENCES users (id)
-    nickname STRING NOT NULL
+    id UUID PRIMARY KEY,
+    ownerid UUID,
+    nickname TEXT NOT NULL,
+    FOREIGN KEY (ownerid) REFERENCES users (id)
 );
-
 CREATE TABLE balances (
-    accountid REFERENCES accounts (id)
-    item STRING NOT NULL -- minecraft:diamond
-    amount INT -- 64 bit signed in cockroachdb 
+    accountid UUID,
+    item TEXT NOT NULL,
+    -- minecraft:diamond
+    amount BIGINT, -- 64 bit int in postgresA,
+    FOREIGN KEY (accountid) REFERENCES accounts (id)
 );
-
 CREATE TABLE transactions (
-    time_millis INT PRIMARY KEY -- milliseconds since 1970
-    from UUID NOT NULL REFERENCES accounts (id)
-    to UUID NOT NULL REFERENCESS accounts (id)
-    amount INT NOT NULL
-    item STRING NOT NULL
-    CONSTRAINT from_to_different CHECK (from != to)
+    id BIGSERIAL PRIMARY KEY,
+    time_millis BIGINT,
+    -- milliseconds since 1970
+    amount BIGINT NOT NULL,
+    item TEXT NOT NULL,
+    fromid UUID,
+    toid UUID,
+    FOREIGN KEY (fromid) REFERENCES accounts (id),
+    FOREIGN KEY (toid) REFERENCES accounts (id),
+    CONSTRAINT from_to_different CHECK (fromid != toid)
 );
+SELECT create_hypertable('transactions', time_millis);
+CREATE INDEX ON transactions (item, time_millis desc);
