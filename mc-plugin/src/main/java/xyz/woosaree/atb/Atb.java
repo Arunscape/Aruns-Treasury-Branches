0107777ce1fe37db8aa8cfc8af13c8562dea1d9c
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-05-14 00:25:36 -0600

    here we go again

---
+++ mc-plugin/src/main/java/xyz/woosaree/atb/Atb.java
@@ -0,0 +1,17 @@
+package xyz.woosaree.atb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Atb extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
