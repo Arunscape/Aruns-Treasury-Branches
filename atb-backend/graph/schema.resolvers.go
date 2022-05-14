508926936ccf8eb5a499d91b652302f9dc9b70ca
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-05-14 00:07:30 -0600

    idk, nuke everything

--- atb-backend/graph/schema.resolvers.go
+++ atb-backend/graph/schema.resolvers.go
@@ -5,25 +5,22 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/Arunscape/Aruns-Treasury-Branches/atb-backend/graph/generated"
 	"github.com/Arunscape/Aruns-Treasury-Branches/atb-backend/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
-}
+func (r *mutationResolver) SignUp(ctx context.Context, input model.NewUser) (*model.User, error) {
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	user := &model.User{
+		UUID:  input.UUID,
+		Email: input.Email,
+	}
+
+	return user, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
-// Query returns generated.QueryResolver implementation.
-func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
-
 type mutationResolver struct{ *Resolver }
-type queryResolver struct{ *Resolver }
