64e9b409322abd18638c6b109ceed4e798312c0c
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-08-03 16:57:27 -0600

    idk what I'm doing

--- atb_frontend/lib/main.dart
+++ atb_frontend/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Arun's Treasury Branches",
       theme: ThemeData(
         // This is the theme of your application.
         //
