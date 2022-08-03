7e8e6e788fa645d8a2ca48599a9fccf3521f3730
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-08-03 17:17:12 -0600

    basic rust example

--- atb_backend/src/main.rs
+++ atb_backend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use tide::Request;
+use tide::prelude::*;
+
+#[derive(Debug, Deserialize)]
+struct Animal {
+    name: String,
+    legs: u16,
+}
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    tide::log::start();
+    let mut app = tide::new();
+    app.at("/orders/shoes").post(order_shoes);
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn order_shoes(mut req: Request<()>) -> tide::Result {
+    let Animal { name, legs } = req.body_json().await?;
+    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
 }
