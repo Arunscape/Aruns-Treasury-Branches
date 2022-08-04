5967aca357d69f6aa4663d89ab1028fa8975aab1
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-08-03 19:58:13 -0600

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- atb_backend/src/main.rs
+++ atb_backend/src/main.rs
@@ -1,22 +1,26 @@
-use tide::Request;
 use tide::prelude::*;
+#[macro_use]
+extern crate dotenv_codegen;
 
-#[derive(Debug, Deserialize)]
-struct Animal {
-    name: String,
-    legs: u16,
-}
+use std::sync::{Arc, RwLock};
+
+use juniper::{http::graphiql, http::GraphQLRequest, RootNode};
+use tide::{http::mime, Body, Redirect, Request, Response, Server, StatusCode};
+
+
+#[derive(Clone)]
+struct State{}
 
 #[async_std::main]
-async fn main() -> tide::Result<()> {
+async fn main() -> std::io::Result<()> {
     tide::log::start();
-    let mut app = tide::new();
-    app.at("/orders/shoes").post(order_shoes);
-    app.listen("127.0.0.1:8080").await?;
+    let mut app = Server::with_state(State {
+        
+    });
+    app.at("/").get(Redirect::permanent("/graphiql"));
+    //app.at("/graphql").post(handle_graphql);
+    //app.at("/graphiql").get(handle_graphiql);
+    app.listen(format!("{}:{}", dotenv!("ADDRESS"), dotenv!("PORT"))).await?;
     Ok(())
 }
 
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-    let Animal { name, legs } = req.body_json().await?;
-    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
-}
