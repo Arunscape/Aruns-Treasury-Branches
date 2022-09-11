3fadba8eb4a4bdbc3c9e615058288bec81cff24c
Arunscape <8227297+Arunscape@users.noreply.github.com>
2022-09-11 01:23:35 -0600

    yay I can generate a EdDSA key now
    don't worry about the private key lol it's not mine
    https://github.com/Keats/jsonwebtoken/blob/af684c146f288d993c632e15fb390f29ebe46683/tests/eddsa/private_ed25519_key.pem

--- atb_backend/atb_backend/src/auth_server.rs
+++ atb_backend/atb_backend/src/auth_server.rs
@@ -1,4 +1,5 @@
 use chrono;
+use chrono::Duration;
 use chrono::offset::Utc;
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use lazy_static::lazy_static;
@@ -23,17 +24,20 @@ lazy_static! {
     static ref AUTH_SERVER_ADDR: String = env::var("AUTH_ADDR").unwrap_or("localhost:8081".into());
     static ref HEADER: Header = Header::new(Algorithm::EdDSA);
     static ref KEY: EncodingKey = {
-        let auth_secret = env::var("AUTH_SECRET").unwrap_or("changeme".into());
-        EncodingKey::from_secret(auth_secret.as_ref())
+        let auth_secret = env::var("AUTH_SECRET").unwrap_or("-----BEGIN PRIVATE KEY-----
+        MC4CAQAwBQYDK2VwBCIEIGrD/e7uKYqSY4twDEsRfMMuLSrODf14dpTiTK6K1YI0
+        -----END PRIVATE KEY-----".into());
+        //EncodingKey::from_secret(&auth_secret.as_bytes())
+        EncodingKey::from_ed_pem(&auth_secret.as_bytes()).unwrap()
     };
 }
 
-fn make_jwt(uuid: &str) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
-    let expiration = Utc::now()
-        .checked_add_signed(
-            // 60s * 10 = 10 minutes
-            chrono::Duration::seconds(60 * 10)
-            ).ok_or("failed to add 10 minutes to current time. I see we have somehow overflowed 64 bit time. Congrats? And hello future aliens!")?
+fn make_jwt(uuid: &str) -> Result<String, jsonwebtoken::errors::Error> {    
+    let expiration = (Utc::now() + Duration::minutes(15))
+        // .checked_add_signed(
+        //     // 60s * 10 = 10 minutes
+        //     chrono::Duration::seconds(60 * 10)
+        //     ).ok_or(jsonwebtoken::errors::Error("failed to add 10 minutes to current time. I see we have somehow overflowed 64 bit time. Congrats? And hello future aliens!".into()))?
         .timestamp();
 
     let claims = Claims {
@@ -41,12 +45,14 @@ fn make_jwt(uuid: &str) -> Result<String, Box<dyn std::error::Error + Send + Syn
         exp: expiration as usize,
     };
 
-    encode(&*HEADER, &claims, &*KEY).map_err(|e| e.into())
+    encode(&*HEADER, &claims, &*KEY)
 }
 
 
 
+
 pub async fn auth_server() -> std::io::Result<()> {
+
     let mut auth_server = tide::new();
 
     auth_server
@@ -68,11 +74,11 @@ pub async fn auth_server() -> std::io::Result<()> {
                 type_of(&token);
             }
 
-            let token = token.map_err(|e| Err(format!("{}", e)))?;
+            let token = token?;
 
             let login_url = format!("https://atb.arun.gg/login/{token}");
 
-            Ok(json!({ "uuid": uuid }))
+            Ok(login_url)
         });
 
     auth_server
