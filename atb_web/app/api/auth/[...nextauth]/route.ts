ac4dae7abb673dca4cd11c3c4b1692d3786c9958
Arunscape <8227297+Arunscape@users.noreply.github.com>
2023-05-11 23:21:59 -0600

    idk

--- atb_web/app/api/auth/[...nextauth]/route.ts
+++ atb_web/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,18 @@
-import NextAuth from "@auth/nextjs"
+import NextAuth from "next-auth"
+import AzureADProvider from "next-auth/providers/azure-ad";
+
+if (!process.env.AZURE_AD_CLIENT_ID) throw new Error("AZURE_AD_CLIENT_ID is not defined")
 
 const handler = NextAuth({
-  ...
+  providers: [
+    AzureADProvider({
+      // clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET || "AZURE_AD_CLIENT_SECRET is not defined",
+      // tenantId: process.env.AZURE_AD_TENANT_ID,
+      clientId: "b65a4f90-a35f-4345-a755-fa4c05c7a8d9",
+      tenantId: "01aee9c7-5d1f-409d-b90a-c21e44a429e5",
+    }),
+  ]
 })
 
 export { handler as GET, handler as POST }
