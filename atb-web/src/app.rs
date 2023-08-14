e11b16db10aa2e496d49060b0b9092ac394e403c
Arunscape <8227297+Arunscape@users.noreply.github.com>
2023-08-13 23:38:57 -0600

    aha it works now

--- atb-web/src/app.rs
+++ atb-web/src/app.rs
@@ -97,99 +97,35 @@ pub async fn res() -> Result<serde_json::Value, ServerFnError> {
    Ok(pong)
 }
 
-async fn load_data(value: i32) -> i32 {
-    // fake a one-second delay
-    use gloo_timers::future::TimeoutFuture;
-    TimeoutFuture::new(1_000).await;
-    value * 10
-}
-
 #[component]
 fn StatusPage(cx: Scope) -> impl IntoView {
 
-    //let s = create_local_resource(
-    let s = create_resource(
-        cx,
-        ||(),
-        |_| async move {res().await}
-    );
+    let (value, set_value) = create_signal(cx, serde_json::Value::Null);
+
+    let once = create_resource(cx, move || value, move |_| async move  { 
+        let r = res().await;
+        let r = r.unwrap_or(serde_json::Value::Null);
+        set_value(r.clone());
+        r
+
+    });
 
 
-    let idk = match s.read(cx) {
-        Some(s) => match s {
-            Ok(s) => s.to_string(),
-            Err(e) => {
-                e.to_string()
-            },
-        }
-        None => "REEEEEEEEEEEEEEEEEE".to_string()
-    };
-
-    let (count, set_count) = create_signal(cx, 0);
-
-    // create_resource takes two arguments after its scope
-    let async_data = create_resource(
-        cx,
-        // the first is the "source signal"
-        count,
-        // the second is the loader
-        // it takes the source signal's value as its argument
-        // and does some async work
-        |value| async move { load_data(value).await },
-    );
-    // whenever the source signal changes, the loader reloads
-
-    // you can also create resources that only load once
-    // just return the unit type () from the source signal
-    // that doesn't depend on anything: we just load it once
-    let stable = create_resource(cx, || (), |_| async move { load_data(1).await });
-
-    // we can access the resource values with .read()
-    // this will reactively return None before the Future has resolved
-    // and update to Some(T) when it has resolved
-    let async_result = move || {
-        async_data
-            .read(cx)
-            .map(|value| format!("Server returned {value:?}"))
-            // This loading state will only show before the first load
-            .unwrap_or_else(|| "Loading...".into())
-    };
-
-    let loading = s.loading();
-    let is_loading = move || if loading() { "Loading..." } else { "Idle." };
-
-    let loading2 = async_data.loading();
-    let is_loading2 = move || if loading2() { "Loading..." } else { "Idle." };
-
-
-    
-
-
-    //println!("Ping result: {:?}", pong);
-    view! { cx, 
-        <h1>"Status Page"</h1> 
-        <div>{is_loading()}</div>
-        <div>{idk}</div>
-        <button
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
-            }
+    view! { cx,
+        <h1>"Server Status"</h1>
+        <Suspense
+        fallback=move || view! { cx, <p>"Loading..."</p> }
         >
-            "Click me"
-        </button>
-        <p>
-            <code>"stable"</code>": " {move || stable.read(cx)}
-        </p>
-        <p>
-            <code>"count"</code>": " {count}
-        </p>
-        <p>
-            <code>"async_value"</code>": "
-            {async_result}
-            <br/>
-            {is_loading2}
-        </p>
+            <div>
+            {
+                move || {
+                    once.read(cx).map(|v| view! { cx, <p>{v.to_string()}</p> })
+                }
+            }
+            </div>
+        </Suspense>
     }
+
 }
 
 #[component]
