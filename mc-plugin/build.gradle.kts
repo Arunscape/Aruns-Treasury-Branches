c28bd9f6620af3fd8e1331d6c444fa8005cadeca
Arunscape <8227297+Arunscape@users.noreply.github.com>
2024-10-23 18:43:33 -0600

    start a new plugin from scratch

--- mc-plugin/build.gradle.kts
+++ mc-plugin/build.gradle.kts
@@ -1,68 +1,21 @@
-//val ktor_version: String by project
-//val kotlin_version: String by project
-
-val kotlin_version = "2.0.0"
-val ktor_version = "latest.release"
-
 plugins {
-    idea
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0"
-    id("com.github.johnrengelman.shadow") version "8.+"
-    "kotlin-dsl"
-
+    kotlin("jvm") version "2.0.20"
 }
+
+group = "gg.arun.atb"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven {
-        name = "purpurmc"
-        url = uri("https://repo.purpurmc.org/snapshots")
-    }
-    maven("https://www.jitpack.io")
 }
 
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
 kotlin {
     jvmToolchain(21)
 }
-
-
-idea {
-    module {
-
-        isDownloadJavadoc = true
-        isDownloadSources = true
-    }
-}
-//allprojects {
-//
-//    dependencies {
-//
-//        implementation("io.ktor:ktor-client-core")
-//        implementation("io.ktor:ktor-client-cio")
-//        implementation("io.ktor:ktor-client-content-negotiation")
-//        implementation("io.ktor:ktor-serialization-kotlinx-json")
-//
-////    implementation(libs.io.ktor.ktor.client.core)
-////    implementation(libs.io.ktor.ktor.client.cio)
-////    implementation(libs.io.ktor.ktor.client.content.negotiation)
-////    implementation(libs.io.ktor.ktor.serialization.kotlinx.json)
-//
-//
-//        implementation(libs.com.github.shynixn.mccoroutine.mccoroutine.bukkit.api)
-//        implementation(libs.com.github.shynixn.mccoroutine.mccoroutine.bukkit.core)
-//
-//        implementation("io.jsonwebtoken:jjwt-api")
-//        runtimeOnly("io.jsonwebtoken:jjwt-impl")
-//        runtimeOnly("io.jsonwebtoken:jjwt-jackson")
-//
-////    implementation(libs.io.jsonwebtoken.jjwt.api)
-////    runtimeOnly(libs.io.jsonwebtoken.jjwt.impl)
-////    runtimeOnly(libs.io.jsonwebtoken.jjwt.jackson)
-//
-//        compileOnly("org.purpurmc.purpur", "purpur-api", "+")
-//
-//    }
-//
-//}
-
